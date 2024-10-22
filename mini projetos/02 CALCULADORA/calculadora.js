f0cf4dd9fe171b85663fa8f28bd819b4f8e75de0
Joenderson Henrique <joendersonchaves1@gmail.com>
2024-10-22 19:06:54 -0300

    inserir decimal

--- mini projetos/02 CALCULADORA/calculadora.js
+++ mini projetos/02 CALCULADORA/calculadora.js
@@ -60,8 +60,46 @@ const selecionarOperador = (evento) => {
 }
 operadores.forEach( operador => operador.addEventListener('click', selecionarOperador));
 
-const resultado = () => {
+const ativarIgual = () => {
     calcular();
     operador = undefined;
 }
-document.getElementById('igual').addEventListener('click', resultado)
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+const limparDisplay = () => display.textContent = '';
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
+    novoNumero = true;
+    numeroAnterior = undefined;
+}
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+const removerUltimoNum = () => {
+    display.textContent = display.textContent.slice(0, -1)
+}
+document.getElementById('backspace').addEventListener('click', removerUltimoNum);
+
+const inverterSinal = () =>{
+    novoNumero = true;
+    atualizarDisplay (display.textContent * -1)
+    
+};  
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+const existeDecimal = () => display.textContent.indexOf(',') != -1;
+const existeValor = () => display.textContent.length > 0;
+
+const inserirDecimal = () =>{
+    if(!existeDecimal()){
+        if(existeValor()){
+            atualizarDisplay(',')
+        }else {
+            atualizarDisplay('0,')
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', inserirDecimal)
+
